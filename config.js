79760a320c6563fe10ed2e1c34fdf6f15bd66a61
Nguyen Duc <thangtaulai206@yahoo.com>
2022-11-09 19:48:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introTitle: 'À nhon a xê ôôôô !!',
+    introDesc: `Trong các loại chúc, thì chúc mừng sinh nhật là Happy birthday.
+    Đề nghị em sinh nhật vui vẻ theo bộ luật hình sự
+    nếu hok vui thì kệ em 😙😙 Happy birthday byba !!!
+    Bấm nút để nhận quà`,
+    btnIntro: 'Nhận quà',
+        title: 'Chọn con tim hay là nghe lý trí chọn Iphone 14 rồ max hay là chọn Anh 🥰🥰',
+    desc: 'Suy nghĩ kĩ hả chọn nha em :))))))',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Iphone 14 pro max :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh đẹp trai, Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
